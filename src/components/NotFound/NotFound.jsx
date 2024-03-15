27cd9d891ccd5fff9b6a3af6408e8eeac3d5b501
Michellebagot <121902516+Michellebagot@users.noreply.github.com>
2024-03-15 17:43:38 +0000

    styling definately finished for friday

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,18 +1,22 @@
 import { Link } from "react-router-dom";
+import "./NotFound.css"
 
 const NotFound = () => {
-    return(
-        <>
-        <h2>Hey, it looks like you're a little lost!</h2>
-        <p>Let's help you find your way!</p>
+  return (
+    <section className="lost">
+      <h2>Hey, it looks like you're a little lost!</h2>
+      <p>Let's help you find your way!</p>
 
-        <Link to='https://www.reddit.com'>Were you looking for the real reddit?</Link>
-        <Link to='/'>Click here to go 'Home'</Link>
-        <Link to='/articles'>Want to view more articles? click here!</Link>
-        <Link to='/topics'>How about some topics?</Link>
-        <Link to={`https://youtu.be/dQw4w9WgXcQ?si=RiJpBan1WvbtMqM6`}>Feeling Brave?</Link>
-        </>
-    )
-}
+      <br></br>
+<p>Where would you like to go? use these links!</p>
+      <Link to="https://www.reddit.com" className="lost-link">
+        Were you looking for the real reddit?
+      </Link>
+      <Link to="/" className="lost-link">Click here to go 'Home'</Link>
+      <Link to="/articles" className="lost-link">Want to view more articles? click here!</Link>
+      <Link to="/topics" className="lost-link">How about some topics?</Link>
+    </section>
+  );
+};
 
-export default NotFound
+export default NotFound;
